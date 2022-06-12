ad64765066f4a53eecd6f82ca83140eb529b5892
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-06-11 22:40:15 -0400

    Day 2 - Pending test

---
+++ lonely-integer/index.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+  // Write your code here
+  var arrayOfElements = [];
+  a.map((item) => {
+    arrayOfElements[item] = arrayOfElements[item]
+      ? arrayOfElements[item] + 1
+      : 1;
+  });
+  const arrayOfLonelyInteger = arrayOfElements.reduce((arr, item, index) => {
+    if (item === 1) arr.push(index);
+    return arr;
+  }, []);
+  return arrayOfLonelyInteger[0];
+}
+
+function main() {
+  const ws = fs.createWriteStream("file.txt");
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const a = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
+
+  const result = lonelyinteger(a);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
---
+++ lonely-integer/readme.md
@@ -0,0 +1,8 @@
+Given an array of integers, where all elements but one occur twice, find the unique element.
+
+Example:
+
+7  
+1 2 3 4 3 2 1
+
+Result: 4.
