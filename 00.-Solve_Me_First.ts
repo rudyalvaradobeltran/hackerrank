edf3bc302aa5f50f62c05f1f169c3f1db1b951fd
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2023-12-30 16:21:58 -0300

    00 Solve me first

---
+++ 00.-Solve_Me_First.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function main() {
+    let a: number = parseInt(readLine());
+    let b: number = parseInt(readLine());
+    solveMeFirst(a, b);
+}
+
+function solveMeFirst(a: number, b: number) {
+    console.log(a + b);
+}
