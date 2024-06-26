88ec09287eebe8c3dd45e2244316edcf6dbf125d
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2024-06-26 16:27:51 -0400

    50 Lisa's workbook V1

---
+++ 50.-Lisa's_Workbook.ts
@@ -0,0 +1,73 @@
+"use strict";
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'workbook' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY arr
+ */
+
+function workbook(n: number, k: number, arr: number[]): number {
+  var array: number[][] = [];
+  var page = 1;
+  var count = 0;
+  for (let i: number = 1; i <= arr.length; i++) {
+    for (let j: number = 1; j <= arr[i-1]; j++) {
+      if (!array[page]) array[page] = [];
+      if (page === j) count++;
+      array[page].push(j);
+      if (j % k === 0 && j !== arr[i-1]) page++;
+    }
+    page++;
+  }
+  return count;
+}
+
+function main() {
+  const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
+
+  const firstMultipleInput: string[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ");
+
+  const n: number = parseInt(firstMultipleInput[0], 10);
+
+  const k: number = parseInt(firstMultipleInput[1], 10);
+
+  const arr: number[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result: number = workbook(n, k, arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
